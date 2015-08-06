1b5c94fefbf0199c616d645728536ea83482dec4
SAVeselovskiy <veselovskiysergey94@gmail.com>
2015-08-06 22:22:54 +0400

    initial commit

---
+++ app/src/main/java/sportplace/su/sportplace_crm/Auth.java
@@ -0,0 +1,30 @@
+package sportplace.su.sportplace_crm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Admin on 06.08.2015.
+ */
+public class Auth extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/sportplace/su/sportplace_crm/SportplaceApplication.java
@@ -0,0 +1,38 @@
+package sportplace.su.sportplace_crm;
+
+import android.app.Application;
+
+import com.crashlytics.android.Crashlytics;
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by Admin on 06.08.2015.
+ */
+public class SportplaceApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fabric.with(this, new Crashlytics());
+        // Initialize Crash Reporting.
+//    ParseCrashReporting.enable(this);
+
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(this, "OSOAgC3VltYk1w7hslj83PUguPq5kC7wSEFqAaaC", "A0ex2zIEFbv27vxsyupe71frkrEvxXeX7xAkmHWL");
+
+
+
+//        ParseUser.enableAutomaticUser();
+//        ParseACL defaultACL = new ParseACL();
+//        // Optionally enable public read access.
+//        // defaultACL.setPublicReadAccess(true);
+//        ParseACL.setDefaultACL(defaultACL, true);
+    }
+}
